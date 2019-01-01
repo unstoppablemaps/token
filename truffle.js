134525b545c722ca4c271192ce006b362ebc308d
chzl-io <46208150+chzl-io@users.noreply.github.com>
2019-01-01 11:43:08 -0600

    Add files via upload

---
+++ truffle.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    live: {
+      host: "178.25.19.88", // Random IP for example purposes (do not use)
+      port: 80,
+      network_id: 1,        // Ethereum public network
+      // optional config values:
+      // gas
+      // gasPrice
+       //from - default address to use for any transaction Truffle makes during migrations
+      // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+      //          - function that returns a web3 provider instance (see below.)
+      //          - if specified, host and port are ignored.
+      // skipDryRun: - true if you don't want to test run the migration locally before the actual migration (default is false)
+      // timeoutBlocks: - if a transaction is not mined, keep retrying for this number of blocks (default is 50),compilers: {
+      solc: {
+        version: "0.4.24",
+      },
+    }
+  }
+};
+
